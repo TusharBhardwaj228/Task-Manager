5dc62b383ac07e63f611f8e9650c9ba530fcb35d
Tushar <t@gmail.com>
2024-06-18 03:28:31 +0530

    Second commit

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 app.get('/',(req,res)=>{
   res.send('Hello');
